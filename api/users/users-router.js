f09264631f9a2633dfd6f14b57150d1dafd21f35
Michael Maton <mjmaton6@gmail.com>
2021-01-30 19:59:46 -0600

    users model/router complete, register and login working

---
+++ api/users/users-router.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const Users = require("./users-model");
+const jwt = require("jsonwebtoken");
+const { jwtSecret } = require("../../config/secrets");
+
+router.get("/", (req, res) => {
+  Users.get()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.post("/register", (req, res) => {
+  const details = req.body;
+
+  const hashedPass = bcrypt.hashSync(details.password, 12);
+  details.password = hashedPass;
+
+  Users.add(details)
+    .then((user) => {
+      res.status(200).json(user);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const checkingUser = await Users.getBy({ username }).first();
+    if (checkingUser && bcrypt.compareSync(password, checkingUser.password)) {
+      const token = generateToken(checkingUser);
+      res.json({ message: "welcome back", token });
+    } else {
+      res.status(401).json({ message: "invalid credentials" });
+    }
+  } catch {
+    res.status(500).json({ message: "username or password incorrect" });
+  }
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    email: user.email,
+  };
+  const options = {
+    expiresIn: "1d",
+  };
+  return jwt.sign(payload, jwtSecret, options);
+}
+
+module.exports = router;
--- data/seeds/01_users.js
+++ data/seeds/01_users.js
@@ -1,7 +1,7 @@
 
 exports.seed = function (knex, Promise) {
       return knex("users").insert([
-        {first_name: "joe", last_name: "mama", email: "j@m.com", username: "joemama", password: "1234" },
-        {first_name: "lebron", last_name: "james", email: "king@james.com", username: "kingjames", password: "1234" },
+        {first_name: "joe", last_name: "mama", email: "j@m.com", username: "joemama", password: "$2a$04$ynf3Vmlw.N6oCOAtwlhid.77RnRTONWk2Cn12Z5GGVfzrVlG6oKg." },
+        {first_name: "lebron", last_name: "james", email: "king@james.com", username: "kingjames", password: "$2a$04$T9E9ljfDfN.j8gGIgsALM.KXC/0HG0K4e0q/hRxriyTsLHOw4n3Zu" },
       ]);
 };
